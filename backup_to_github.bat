1091fcffca5e940a697eb62503f2c95e552e61ad
Developer <developer@local.host>
2026-03-23 14:27:35 +0800

    Auto backup 2026/03/23

--- backup_to_github.bat
+++ backup_to_github.bat
@@ -4,6 +4,10 @@ cd /d "C:\Users\User\spotify project"
 :: Get today's date
 set TODAY=%date:~0,10%
 
+:: Copy reports to Obsidian Vault
+if not exist "C:\Users\User\Documents\Obsidian Vault\.obsidian\spotify-reports" mkdir "C:\Users\User\Documents\Obsidian Vault\.obsidian\spotify-reports"
+xcopy "C:\Users\User\spotify project\data\reports\*.*" "C:\Users\User\Documents\Obsidian Vault\.obsidian\spotify-reports\" /Y /D
+
 :: Git backup
 git add .
 git commit -m "Auto backup %TODAY%"
